ab6bd27c8b43b47696f20ef8f8cbcdfcdd51bd90
Q4ZWSX <99418850+Q4ZWSX@users.noreply.github.com>
2024-01-21 21:45:58 -0500

    Update calendar.tsx with quickfix
    Calendar not embedded because microsoft hates iframes

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,26 +1,28 @@
-/* eslint-disable react/iframe-missing-sandbox */
 import React from 'react';
-import {Typography, Stack} from '@mui/material';
-import {globalContext, siteNavigation} from '../src/config';
+import { Link, Typography, Box } from '@mui/material';
+import { globalContext } from '../src/config';
 
 export default function Events() {
-	const context = React.useContext(globalContext);
+    const context = React.useContext(globalContext);
 
-	/* I don't like the Google Calendar iframe. In the future, we should move away from iframes. */
-	const calendar = context.isDesktop
-		? <iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&title=CyberVT%20Events&showTitle=0&showPrint=0&showTabs=1&showCalendars=0&mode=MONTH&src=Y19ycTk5Y3JoOTduaWw3YW50dW81ajBkczhrNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F09300' style={{borderWidth: 0}} width='100%' height='600' frameBorder='0' scrolling='no'/>
-		: <iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&title=CyberVT%20Events&showTitle=0&showPrint=0&showTabs=0&showCalendars=0&mode=AGENDA&src=Y19ycTk5Y3JoOTduaWw3YW50dW81ajBkczhrNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F09300' style={{borderWidth: 0}} width='100%' height='600' frameBorder='0' scrolling='no'/>;
+    const url = 'https://outlook.office365.com/owa/calendar/3edc428e321341ddbecc68b81c4cbbba@vt.edu/f02bab09096e422ba69eb018532c5bca9370545146257834267/calendar.html';
+    const message = 'Office365 does not support iframe which broke this calendar, please click on the link above to view it on another page.'
 
-	return (
-		<Stack spacing={2}>
-			<Typography>
-				CyberVT meets weekly in accordance with the Virginia Tech academic year. Below is a calendar of upcoming events, including Zoom links for virtual meetings.
-			</Typography>
-			{calendar}
-		</Stack>
-	);
-}
-
-export async function getStaticProps() {
-	return {props: siteNavigation.calendar};
-}
+    return (
+      <Box 
+        sx={{
+          display: 'flex',
+          flexDirection: 'column', 
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Link href={url} target="_blank" rel="noopener" variant="h6">
+             View Calendar
+        </Link>
+        <Typography variant="body1" color='textSecondary'>
+            {message}
+        </Typography>
+      </Box>
+    )      
+}           
